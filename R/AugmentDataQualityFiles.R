088477f989b9f78d273d83784d5a4ee03b1ec751
Mikhail-iontsev <mikhail_iontsev@icloud.com>
2024-08-08 20:16:53 +0700

    feat: Data quality delta report

---
+++ R/AugmentDataQualityFiles.R
@@ -0,0 +1,97 @@
+# @file augmentDataQualityFiles.R
+#
+#
+# Copyright 2021 Observational Health Data Sciences and Informatics
+#
+# This file is part of AresIndexer
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+#' Augment Data Quality Files
+
+#' @name augmentDataQualityFiles
+#' @details Updates dq-result.json files with information on changes from the previous release
+#'
+#' @param sourceFolders A vector of folder locations that contain the files
+#' exported from Achilles in the ARES Option format (Achilles::exportAO)
+#' to be included in the network data quality index.
+#'
+#'
+#'
+#'
+#' @export
+#'
+
+
+library(jsonlite)
+library(dplyr)
+
+loadDataQualityFile <- function(dir) {
+  filePath <- file.path(dir, "dq-result.json")
+  fromJSON(filePath)
+}
+
+augmentDataQualityFiles <- function(sourceFolders) {
+  for(sourceFolder in sourceFolders) {
+    writeLines(paste0('Augmenting data quality files for: ', basename(sourceFolder)))
+    releases <- list.dirs(sourceFolder, recursive = FALSE)
+    loadedData <- list()
+
+    for (i in seq_along(releases)) {
+      currentReleaseName <- basename(releases[[i]])
+      writeLines(paste0('Processing issues delta for release: ',currentReleaseName))
+      currentQualityFile <- loadDataQualityFile(releases[i])
+      currentChecks <- currentQualityFile$CheckResults
+
+      loadedData[[length(loadedData) + 1]] <- currentChecks
+
+      if (length(loadedData) > 1) {
+        previousData <- loadedData[[length(loadedData) - 1]]
+
+        commonCols <- intersect(names(currentChecks), names(previousData))
+        currentChecks <- currentChecks[, commonCols, drop = FALSE]
+        previousData <- previousData[, commonCols, drop = FALSE]
+
+        mergedData <- currentChecks %>%
+          left_join(previousData, by = "checkId", suffix = c("", "_previous"))
+
+        mergedData <- mergedData %>%
+          mutate(Status = case_when(
+            is.na(FAILED_previous) & FAILED == 1 ~ "NEW",
+            FAILED == 1 & FAILED_previous == 0 ~ "NEW",
+            FAILED == 1 & FAILED_previous == 1 ~ "EXISTING",
+            FAILED == 0 & FAILED_previous == 1 ~ "RESOLVED",
+            FAILED == 0 & FAILED_previous == 0 ~ "STABLE",
+            TRUE ~ "STABLE" # Default case if none of the above match
+          )) %>%
+          select(-ends_with("_previous"))
+
+        currentQualityFile$CheckResults <- mergedData
+      } else {
+        currentChecks <- currentChecks %>%
+          mutate(Status = ifelse(FAILED == 1, "NEW", "STABLE"))
+
+        currentQualityFile$CheckResults <- currentChecks
+      }
+
+      write_json(currentQualityFile, file.path(releases[i], "updated_dq-result.json"))
+
+      #write_json(current_json, file.path(releases[i], "dq-result.json"))
+
+      # Maintain only the last two loaded datasets
+      if (length(loadedData) > 2) {
+        loadedData <- loadedData[-1]
+      }
+    }
+  }
+}
---
+++ R/BuildSourceDataQualityDelta.R
@@ -0,0 +1,66 @@
+# @file BuildSourceDataQualityDelta.R
+#
+#
+# Copyright 2021 Observational Health Data Sciences and Informatics
+#
+# This file is part of AresIndexer
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+#' Build Source Data Quality Delta
+
+#' @details Generates data quality delta file that contains information on changes of issues between releases
+#'
+#' @param sourceFolders A vector of folder locations that contain the files
+#' exported from Achilles in the ARES Option format (Achilles::exportAO)
+#' to be included in the network data quality index.
+#' @import jsonlite
+
+#' @export
+
+buildSourceDataQualityDelta <- function(sourceFolders) {
+  statusCountsTemplate <- c(NEW = 0, EXISTING = 0, RESOLVED = 0, STABLE = 0)
+
+  countStatuses <- function(dirPath) {
+    statusCounts <- statusCountsTemplate
+
+    dataQualityPath <- file.path(dirPath, "updated_dq-result.json")
+    if (file.exists(dataQualityPath)) {
+      data <- fromJSON(dataQualityPath)
+
+      if (!is.null(data$CheckResults)) {
+        statuses <- data$CheckResults$Status
+        statusCounts <- statusCounts + table(factor(statuses, levels = names(statusCountsTemplate)))
+      }
+    }
+
+    return(statusCounts)
+  }
+
+  for(sourceFolder in sourceFolders) {
+    writeLines(paste0('Building source quality delta for: ', sourceFolder))
+    releases <- list.dirs(sourceFolder, full.names = TRUE, recursive = FALSE)
+    sourceCounts <- data.frame()
+    for (release in releases) {
+      releaseName <- basename(release)
+      counts <- countStatuses(release)
+      counts <- c(releaseName, as.numeric(counts))
+
+      # Create a data frame with correct column names
+      countsDf <- as.data.frame(t(counts), stringsAsFactors = FALSE)
+      colnames(countsDf) <- c("Release", names(statusCountsTemplate))
+      sourceCounts <- rbind(sourceCounts, countsDf)
+    }
+    write.csv(sourceCounts, file = file.path(sourceFolder, 'data-quality-delta.csv'), row.names = FALSE)
+  }
+}
